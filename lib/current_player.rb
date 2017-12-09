fa443be83295f329a6431ebc1a595e0bbb22d800
Erica Murdock-Waters <ericahpa@gmail.com>
2017-12-09 19:11:22 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -21,5 +21,5 @@ end
 #end
 
 # refactored original #turn_count (lines 15 - 22) using ternary operator
-def current_player(board) turn_count(board) % 2 ? "X" : "O"
+def current_player(board) turn_count(board) % 2 == 0 ? "X" : "O"
 end
