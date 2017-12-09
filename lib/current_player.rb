225eabb212ed11b62293b1fddf1b52528d89f95e
Erica Murdock-Waters <ericahpa@gmail.com>
2017-12-09 19:06:30 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -21,6 +21,14 @@ end
 #  end
 #end
 
-# refactored original #turn_count (lines 16 - 22) using ternary operator
-def current_player(board) turn_count(board).even? ? "X" : "O"
+def current_player(board)
+  if turn_count(board) % 2 != 0 
+    "X"
+  else
+    "O"
+  end
 end
+
+# refactored original #turn_count (lines 16 - 22) using ternary operator
+#def current_player(board) turn_count(board).even? ? "X" #: "O"
+#end
